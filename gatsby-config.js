4ddaaca879a27b5b25b75726ef3da68226bc2c8e
abumalick <abdullah.hilson@gmail.com>
2019-07-29 09:14:15 +0000

    feat: Allow the use of typescript files for gatsby-node, gatsby-browser and gatsby-ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
+// Allow the use of typescript files for gatsby-node, gatsby-browser and gatsby-ssr
 require('source-map-support').install();
-require('ts-node').register();
+require('ts-node').register({
+  compilerOptions: {
+    allowJs: true, // Allow tsx extension
+    jsx: 'preserve', // JSX will be transpiled by babel afterwards
+    target: 'esnext', // Bebel will transpile
+  },
+});
 
 module.exports = {
   plugins: ['gatsby-plugin-typescript'],
